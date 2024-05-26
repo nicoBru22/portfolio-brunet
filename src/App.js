4a47f75fb068e81a87720aa80542e30a2fd869d7
nicoBru22 <brunet.nicolas35@gmail.com>
2024-05-26 19:26:58 +0200

    erreur de suppression

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import AppRoutes from '../src/routes/routes';
-import MetaData from './components/MetaData';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <MetaData />
       <AppRoutes />
     </div>
   );

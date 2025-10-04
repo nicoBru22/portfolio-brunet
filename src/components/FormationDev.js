e4094c236b5928c0ef9a79080c072c7c26bacf74
nicoBru22 <brunet.nicolas35@gmail.com>
2025-10-04 17:31:07 +0200

    modif pour ajout de la nouvelle formation obtenue

--- src/components/FormationDev.js
+++ src/components/FormationDev.js
@@ -8,6 +8,11 @@ function FormationDev() {
                 intitule={"Développeur Web (Bac+2)"}
                 organisme={"Open Classroom"}
                 periode={"2023-2024"}/>
+            <ElementFormation 
+                intitule={"Développeur Java (Bac+3)"}
+                organisme={"Open Classroom"}
+                periode={"2024-2025"}/>
+
         </div>
     );
 }
